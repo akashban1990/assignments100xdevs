326fb4dd6431f1405810e7d0aa37b74a33f59419
akashban1990 <akashban1990@gmail.com>
2024-03-30 23:55:03 -0400

    addding the expenditure code

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -14,7 +14,36 @@
 */
 
 function calculateTotalSpentByCategory(transactions) {
-  return [];
-}
+  // Create an empty object to store the total spent by category
+  let totalSpentByCategory = {};
 
+  // Iterate over each transaction
+  transactions.forEach(transactions => {
+      // If the category is not yet in the object, add it with the price as the value
+      if (!totalSpentByCategory[transactions.category]) {
+          totalSpentByCategory[transactions.category] = transactions.price;
+      } else {
+          // If the category is already in the object, add the price to the existing value
+          totalSpentByCategory[transactions.category] += transactions.price;
+      }
+  });
+
+  // Convert the object to an array of objects with the format { category: 'Food', totalSpent: 10 }
+  let result = Object.keys(totalSpentByCategory).map(category => {
+      return { category: category, totalSpent: totalSpentByCategory[category] };
+  });
+
+  return result;
+}
 module.exports = calculateTotalSpentByCategory;
+
+
+testtran=[{
+  id: 1,
+  timestamp: 1656076800000,
+  price: 10,
+  category: 'Food',
+  itemName: 'Pizza',
+},]
+
+console.log(calculateTotalSpentByCategory(testtran));

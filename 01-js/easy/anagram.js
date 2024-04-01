c346c98436a72a6808d954423871fe4f59e39d51
akashban1990 <akashban1990@gmail.com>
2024-03-31 21:39:22 -0400

    updated the anagram assignment

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -6,6 +6,27 @@
 
 function isAnagram(str1, str2) {
 
+  if  (str1.length !== str2.length) {
+    return false;
+  }
+
+
+
+  let colstr1 = str1.replace(/[^a-z]/gi, '').toLowerCase();
+  let cleanedStr1 = colstr1.replace(/[^\w\s]/gi, '');
+
+  let colstr2 = str2.replace(/[^a-z]/gi, '').toLowerCase();
+  let cleanedStr2 = colstr2.replace(/[^\w\s]/gi, '');
+
+  let sortedStr1 = cleanedStr1.split('').sort().join('');
+  let sortedStr2 = cleanedStr2.split('').sort().join('');
+
+  if (sortedStr1 === sortedStr2) {
+    return true;
+  } else {
+    return false;
+  }
+
 }
 
 module.exports = isAnagram;
